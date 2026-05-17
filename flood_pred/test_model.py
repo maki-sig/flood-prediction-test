37093b663b81262b96ded312e223c3f0e240f099
maki-sig <mnbotis.nhcs@gmail.com>
2026-05-17 19:49:19 +0800

    Model is trained to predict flood in Naga City. However, flood level is another discussion. Current output is 1s and 0s

--- flood_pred/test_model.py
+++ flood_pred/test_model.py
@@ -5,9 +5,9 @@ predict_flood = joblib.load("flood_pred_model.pkl")
 print("Model loaded")
 
 # parameters to predict
-rainfall_intensity = 15.0
-accum_6h = 65.0
-accum_24h = 140.0
+rainfall_intensity = 12.5
+accum_6h = 12.5
+accum_24h = 12.5
 
 rainfall_data = [[rainfall_intensity, accum_6h, accum_24h]]
 
@@ -22,4 +22,4 @@ prediction = predict_flood.predict(rainfall_data)
 if(prediction == 1):
     print(f"Result:\t{prediction} - yes it will flood")
 else:
-    print(f"Result:\t{prediction} - it will now flood")
+    print(f"Result:\t{prediction} - it will not flood")
